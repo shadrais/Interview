427b2b2004bfa75b346844d3f937da2f4eeff7f2
Shad Rais <shadrais321@outlook.com>
2022-05-23 20:48:38 +0530

    Fixed Style

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,9 +1,14 @@
 import "./styles.css"
 
 const Card = ({ item }) => {
+  const backgroundStyle = {
+    backgroundImage: `linear-gradient(to bottom , rgba(76, 0, 255, 0))`,
+  }
+
   return (
     <div className="card">
-      <img src={item.avatar} alt="imagess" />
+      {/* <div style={backgroundStyle}></div> */}
+      <img style={backgroundStyle} src={item.avatar} alt="imagess" />
       <div className="placeHolder">{item.first_name}</div>
       <div className="text">{item.email}</div>
     </div>
